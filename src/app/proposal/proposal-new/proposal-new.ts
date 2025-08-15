caf34e00578d48dce1d8ac34c1830a375aa93396
Katie Loesch <catherine.loesch@protonmail.com>
2025-08-15 05:37:56 +0100

    add a form to proposal-new component

--- src/app/proposal/proposal-new/proposal-new.ts
+++ src/app/proposal/proposal-new/proposal-new.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Proposal } from '../proposal';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-proposal-new',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './proposal-new.html',
   styleUrl: './proposal-new.scss',
 })
