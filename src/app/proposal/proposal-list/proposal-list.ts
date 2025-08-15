e957623047be485c65b9788efdb47ca30bc9a284
Katie Loesch <catherine.loesch@protonmail.com>
2025-08-15 03:52:30 +0100

    add mock data to proposal-list component

--- src/app/proposal/proposal-list/proposal-list.ts
+++ src/app/proposal/proposal-list/proposal-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from '../proposal';
 
 @Component({
   selector: 'app-proposal-list',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './proposal-list.html',
   styleUrl: './proposal-list.scss',
 })
-export class ProposalList {}
+export class ProposalList {
+  proposalOne: Proposal = new Proposal(
+    13,
+    'Wayne Enterprises',
+    'https://batman.fandom.com/wiki/Wayne_Enterprises',
+    'Ruby on Rails',
+    150,
+    120,
+    15,
+    'lucius.fox@email.com'
+  );
+  proposalTwo: Proposal = new Proposal(
+    140,
+    'LuthorCorp',
+    'https://dcuniverse.fandom.com/wiki/LuthorCorp',
+    'NodeJS',
+    150,
+    120,
+    15,
+    'eve.teschmacher@email.com'
+  );
+  proposalThree: Proposal = new Proposal(
+    415,
+    'Queen Consolidated',
+    'https://arrow.fandom.com/wiki/Queen_Consolidated',
+    'python3',
+    150,
+    120,
+    15,
+    'oliver.queen@email.com'
+  );
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree,
+  ];
+}
