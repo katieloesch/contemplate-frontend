9ba0c124c4e0924adbb5e99043954dd28c95c189
Katie Loesch <catherine.loesch@protonmail.com>
2025-08-15 01:20:01 +0100

    add route for homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Homepage } from './homepage/homepage';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('contemplate-frontend');
